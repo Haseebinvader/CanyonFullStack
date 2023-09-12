6b89483d9689e13e9b4b91babba7f5f387f16710
Haseebinvader <rhaseeb741@gmail.com>
2023-09-12 04:33:54 -0700

    changes

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-# Serializers define the API representation.
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Product
-        fields = ['id','ItemNo','qnty', 'price','HighTemperature', 'Description', 'Description2', 'SearchDescription','Blocked', 'CompoundNumber', 'Material','Durometer','DurometerScale','DurometerRange','Color','LowTemperature','FDACompliant','MaterialSubtype','Brand','MaterialNotes','CrossSectionalGeometry','CrossSectionalDiameter','InsideDiameter','SizeAS568','SizeMetric','SizeJIS','SizeStandard','Online']
